93dbac741b722c52c01e494c564ddbc74ebf1896
Dominika Ejsmont <d.ejsmont95@gmail.com>
2023-03-05 15:55:03 +0100

    Add method addToCart, add(menuProduct), prepareCartProduct and prepareCartProductParams

--- src/js/script.js
+++ src/js/script.js
@@ -174,6 +174,7 @@
       thisProduct.cartButton.addEventListener('click', function (event) {
         event.preventDefault();
         thisProduct.processOrder();
+        thisProduct.addToCart();
       });
     }
 
@@ -226,7 +227,50 @@
         }
       }
       price *= thisProduct.amountWidget.value;
+      thisProduct.priceSingle = thisProduct.priceElem;
       thisProduct.priceElem.innerHTML = price;
+    }
+    addToCart() {
+      const thisProduct = this;
+      app.cart.add(thisProduct.prepareCartProduct);
+    }
+    prepareCartProduct() {
+      const thisProduct = this;
+
+      const productSummary = {
+        id: thisProduct.id,
+        name: thisProduct.data.name,
+        amount: thisProduct.amountWidget.value,
+        priceSingle: thisProduct.priceSingle,
+        price: thisProduct.priceSingle * thisProduct.amountWidget.value,
+        params: thisProduct.prepareCartProductParams,
+      };
+      return;
+    }
+    prepareCartProductParams() {
+      const thisProduct = this;
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+
+      const params = {};
+
+      for (let paramId in thisProduct.data.params) {
+        const param = thisProduct.data.params[paramId];
+
+        params[paramId] = {
+          label: param.label,
+          options: {},
+        };
+        for (let optionId in param.options) {
+          const option = param.options[optionId];
+          const optionSelected =
+            formData[paramId] && formData[paramId].includes(optionId);
+          if (optionSelected) {
+            params[paramId].options.add(option);
+          }
+        }
+      }
+      return params;
     }
   }
 
@@ -316,6 +360,10 @@
       thisCart.dom.toggleTrigger.addEventListener('click', function () {
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
       });
+    }
+    add(menuProduct) {
+      const thisCart = this;
+      console.log('adding product: ', menuProduct);
     }
   }
 
