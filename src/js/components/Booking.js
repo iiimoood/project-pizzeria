cdc9893f15b86239fe23ba05ddcf854b0ae1252e
Dominika Ejsmont <d.ejsmont95@gmail.com>
2023-03-19 12:33:21 +0100

    Add function getData and content inside

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,7 +1,8 @@
-import { templates, select } from '../settings.js';
+import { templates, select, settings } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
+import utils from '../utils.js';
 
 class Booking {
   constructor(element) {
@@ -9,6 +10,58 @@ class Booking {
 
     thisBooking.render(element);
     thisBooking.initWidgets();
+    thisBooking.getData();
+  }
+
+  getData() {
+    const thisBooking = this;
+
+    const startDateParam =
+      settings.db.dateStartParamKey +
+      '=' +
+      utils.dateToStr(thisBooking.datePicker.minDate);
+
+    const endDateParam =
+      settings.db.dateEndParamKey +
+      '=' +
+      utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const params = {
+      bookings: [startDateParam, endDateParam],
+      eventsCurrent: [settings.db.notRepeatParam, startDateParam, endDateParam],
+      eventsRepeat: [settings.db.repeatParam, endDateParam],
+    };
+    // console.log('getData.params', params);
+
+    const urls = {
+      bookings:
+        settings.db.url +
+        '/' +
+        settings.db.bookings +
+        '?' +
+        params.bookings.join('&'),
+      eventsCurrent:
+        settings.db.url +
+        '/' +
+        settings.db.events +
+        '?' +
+        params.eventsCurrent.join('&'),
+      eventsRepeat:
+        settings.db.url +
+        '/' +
+        settings.db.events +
+        '?' +
+        params.eventsRepeat.join('&'),
+    };
+    fetch(urls.bookings)
+      .then(function (bookingsResponse) {
+        return bookingsResponse.json();
+      })
+      .then(function (bookings) {
+        console.log(bookings);
+      });
+
+    // console.log('urls:', urls);
   }
   render(bookingWidgetContainer) {
     const thisBooking = this;
