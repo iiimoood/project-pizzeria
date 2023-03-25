8dac5c4b9aea364fcf9a2d365ac6796caec61025
Dominika Ejsmont <d.ejsmont95@gmail.com>
2023-03-25 09:50:37 +0100

    Change content for using replit

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -111,7 +111,10 @@ export const settings = {
     tableIdAttribute: 'data-table',
   },
   db: {
-    url: '//localhost:3131',
+    url:
+      '//' +
+      window.location.hostname +
+      (window.location.hostname == 'localhost' ? ':3131' : ''),
     products: 'products',
     orders: 'orders',
     bookings: 'bookings',
